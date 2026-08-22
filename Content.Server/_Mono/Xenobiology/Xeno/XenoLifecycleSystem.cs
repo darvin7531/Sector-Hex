6a84290f6e07d396fe0df6dc97ad20458e14d53b
darvin7531 <srrafale@gmail.com>
2026-08-22 23:20:43 +0300

    feat(xenobiology): implement first xeno lifecycle

---
+++ Content.Server/_Mono/Xenobiology/Xeno/XenoLifecycleSystem.cs
@@ -0,0 +1,127 @@
+using Content.Shared._Mono.Xenobiology.Xeno;
+using Content.Shared.Interaction;
+using Content.Shared.Interaction.Events;
+using Robust.Shared.Timing;
+
+namespace Content.Server._Mono.Xenobiology.Xeno;
+
+public sealed class XenoLifecycleSystem : EntitySystem
+{
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<XenoEggComponent, UseInHandEvent>(OnEggUse);
+        SubscribeLocalEvent<XenoEggComponent, ActivateInWorldEvent>(OnEggActivate);
+        SubscribeLocalEvent<XenoParasiteComponent, InteractHandEvent>(OnParasiteInteract);
+    }
+
+    private void OnEggUse(Entity<XenoEggComponent> egg, ref UseInHandEvent args)
+    {
+        if (args.Handled || !TryPlace(egg))
+            return;
+
+        _transform.SetCoordinates(egg, Transform(args.User).Coordinates);
+        args.Handled = true;
+    }
+
+    private void OnEggActivate(Entity<XenoEggComponent> egg, ActivateInWorldEvent args)
+    {
+        if (args.Handled || !TryOpen(egg))
+            return;
+
+        args.Handled = true;
+    }
+
+    private void OnParasiteInteract(Entity<XenoParasiteComponent> parasite, ref InteractHandEvent args)
+    {
+        if (!args.Handled && TryInfect(parasite, args.User))
+            args.Handled = true;
+    }
+
+    public bool TryPlace(Entity<XenoEggComponent> egg)
+    {
+        if (egg.Comp.State != XenoEggState.Item || egg.Comp.PlacementAt != null)
+            return false;
+
+        egg.Comp.PlacementAt = _timing.CurTime + egg.Comp.PlacementDelay;
+        Dirty(egg);
+        return true;
+    }
+
+    public bool TryOpen(Entity<XenoEggComponent> egg)
+    {
+        if (egg.Comp.State != XenoEggState.Grown)
+            return false;
+
+        egg.Comp.State = XenoEggState.Opening;
+        egg.Comp.OpenAt = _timing.CurTime + egg.Comp.OpeningDelay;
+        Dirty(egg);
+        return true;
+    }
+
+    public bool TryInfect(EntityUid parasite, EntityUid host)
+    {
+        if (!HasComp<XenoParasiteComponent>(parasite) ||
+            !TryComp(host, out InfectableHostComponent? infectable) ||
+            HasComp<XenoInfectionComponent>(host))
+        {
+            return false;
+        }
+
+        var infection = AddComp<XenoInfectionComponent>(host);
+        infection.SpawnAt = _timing.CurTime + infectable.IncubationDelay;
+        infection.LarvaPrototype = infectable.LarvaPrototype;
+        Dirty(host, infection);
+        return true;
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+        var now = _timing.CurTime;
+
+        var eggs = EntityQueryEnumerator<XenoEggComponent>();
+        while (eggs.MoveNext(out var uid, out var egg))
+        {
+            if (egg.State == XenoEggState.Item && egg.PlacementAt <= now)
+            {
+                egg.PlacementAt = null;
+                egg.State = XenoEggState.Growing;
+                egg.GrowAt = now + egg.GrowthDelay;
+                _transform.AnchorEntity(uid, Transform(uid));
+                Dirty(uid, egg);
+                continue;
+            }
+
+            if (egg.State == XenoEggState.Growing && egg.GrowAt <= now)
+            {
+                egg.GrowAt = null;
+                egg.State = XenoEggState.Grown;
+                Dirty(uid, egg);
+                continue;
+            }
+
+            if (egg.State != XenoEggState.Opening || egg.OpenAt > now)
+                continue;
+
+            egg.OpenAt = null;
+            egg.SpawnedParasite ??= Spawn(egg.ParasitePrototype, Transform(uid).Coordinates);
+            egg.State = XenoEggState.Opened;
+            Dirty(uid, egg);
+        }
+
+        var infections = EntityQueryEnumerator<XenoInfectionComponent>();
+        while (infections.MoveNext(out var uid, out var infection))
+        {
+            if (infection.SpawnedLarva != null || infection.SpawnAt > now)
+                continue;
+
+            infection.SpawnedLarva = Spawn(infection.LarvaPrototype, Transform(uid).Coordinates);
+            Dirty(uid, infection);
+        }
+    }
+}
---
+++ Content.Shared/_Mono/Xenobiology/Xeno/XenoLifecycleComponents.cs
@@ -0,0 +1,73 @@
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+
+namespace Content.Shared._Mono.Xenobiology.Xeno;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true), AutoGenerateComponentPause]
+public sealed partial class XenoEggComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public XenoEggState State;
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan PlacementDelay = TimeSpan.FromSeconds(2);
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan GrowthDelay = TimeSpan.FromMinutes(2);
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan OpeningDelay = TimeSpan.FromSeconds(1);
+
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoNetworkedField, AutoPausedField]
+    public TimeSpan? PlacementAt;
+
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoNetworkedField, AutoPausedField]
+    public TimeSpan? GrowAt;
+
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoNetworkedField, AutoPausedField]
+    public TimeSpan? OpenAt;
+
+    [DataField, AutoNetworkedField]
+    public EntProtoId ParasitePrototype = "MonoXenoParasite";
+
+    [DataField, AutoNetworkedField]
+    public EntityUid? SpawnedParasite;
+}
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class XenoParasiteComponent : Component;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class InfectableHostComponent : Component
+{
+    [DataField]
+    public TimeSpan IncubationDelay = TimeSpan.FromMinutes(8);
+
+    [DataField]
+    public EntProtoId LarvaPrototype = "MonoXenoLarva";
+}
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true), AutoGenerateComponentPause]
+public sealed partial class XenoInfectionComponent : Component
+{
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoNetworkedField, AutoPausedField]
+    public TimeSpan SpawnAt;
+
+    [DataField, AutoNetworkedField]
+    public EntProtoId LarvaPrototype = "MonoXenoLarva";
+
+    [DataField, AutoNetworkedField]
+    public EntityUid? SpawnedLarva;
+}
+
+[Serializable, NetSerializable]
+public enum XenoEggState : byte
+{
+    Item,
+    Growing,
+    Grown,
+    Opening,
+    Opened,
+}
